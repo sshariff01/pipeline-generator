4ff9d1c750bc19da08aea0c9652705e67595587a
Shoabe Shariff <sshariff@pivotal.io>
2016-02-04 12:35:39 -0500

    Make specs cleaner

--- spec/plan_spec.rb
+++ spec/plan_spec.rb
@@ -56,17 +56,7 @@ describe Pipeline::Job::Plan do
 
       expect(resource.get_hash).to have_key("passed")
       expect(resource.get_hash).to have_key("trigger")
-      expect(plan.get_hash).to eq([
-        {
-          "get" => "dummy-resource-name",
-          "passed" => ['passed-some-job'],
-          "trigger" => true,
-          "params" => {
-              'first-key' => 'first-val',
-              'second-key' => 'second-val'
-          }
-        }
-      ])
+      expect(plan.get_hash).to eq([get_resource_hash])
     end
   end
 
@@ -77,45 +67,17 @@ describe Pipeline::Job::Plan do
       plan.add_put(resource)
 
       expect(resource.get_hash).to have_key("resource")
-      expect(plan.get_hash).to eq([
-        {
-          "put" => "dummy-resource-name",
-          "resource" => "dummy",
-          "params" => {
-              'first-key' => 'first-val',
-              'second-key' => 'second-val'
-          }
-        }
-      ])
+      expect(plan.get_hash).to eq([put_resource_hash])
     end
   end
 
   context "when defining a task(s)" do
-    it "successfully inserts the task to the plan with keys: get, passed, trigger, params: task name, config" do
+    it "successfully inserts a single task to the plan with keys: get, passed, trigger, params: task name, config" do
       allow(task).to receive(:get_hash) { task_hash }
 
       plan.add_task(task)
 
-      expect(plan.get_hash).to eq([
-        {
-          "task" => "dummy-task-name",
-          "privileged" => true,
-          "config" => {
-            'platform' => 'some-platform',
-            'image' => 'some-image',
-            'inputs' => [
-              {
-                'name' => 'input1',
-                'path' => 'input1/path'
-              }
-            ],
-            'run' => {
-              'path' => 'repo/scripts/test',
-              'args' => ['arg0', 'arg1']
-            }
-          }
-        }
-      ])
+      expect(plan.get_hash).to eq([task_hash])
     end
   end
 end
